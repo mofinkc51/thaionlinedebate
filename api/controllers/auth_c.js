3bee1f406cdae6e31a4275a79a6b66840496b509
mofinkc51 <82575774+mofinkc51@users.noreply.github.com>
2023-09-05 14:54:09 +0700

    register_db_v1

--- api/controllers/auth_c.js
+++ api/controllers/auth_c.js
@@ -3,23 +3,28 @@ import bcrypt from "bcryptjs"
 export const register = (req,res)=>{
     //check user
 
-    const q = "SELECT * FROM users WHERE username = ?"
+    const q = "SELECT * FROM user WHERE user_name = ?"
 
-    db.query(q,[req.body.username], (err,data)=>{
+    db.query(q,[req.body.user_name], (err,data)=>{
         if(err) return res.status(500).json(err)
         if(data.length) return res.status(409).json("User already ext")
     
     //create user
         //hash pass
         const salt = bcrypt.genSaltSync(10);
-        const hashedPassword = bcrypt.hashSync(req.body.password,salt)
+        const hashedPassword = bcrypt.hashSync(req.body.user_password,salt)
         
-        const q = "INSERT INTO users (`username`,`email`,`phonenum`,`password`) VALUE (?)";
+        const q = "INSERT INTO user (`user_id`,`user_name`,`user_email`,`user_phonenum`,`user_password`) VALUE (?)";
+        
+        const randomId = function(length = 6) {
+            return Math.random().toString(36).substring(2, length+2);
+          };
 
         const values = [
-            req.body.username,
-            req.body.email,
-            req.body.phonenum,
+            randomId(8),
+            req.body.user_name,
+            req.body.user_email,
+            req.body.user_phonenum,
             hashedPassword
         ];
         db.query(q, [values], (err, data) => {
