9706ff318164aeb5858dba1560657d71fa814f4d
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-09-30 17:11:40 +0700

    making debate topic page
    - change font
    - create tag component
    - start making Debate Topic page

--- thai-online-debate/src/pages/debate-topic-page/DebateTopic.jsx
+++ thai-online-debate/src/pages/debate-topic-page/DebateTopic.jsx
@@ -1,12 +1,92 @@
 import React from 'react'
+import './DebateTopic.css'
 import UserNavbar from '../../components/Navbar/UserNavBar'
+import TopicTag from '../../components/TopicTag'
+import optionButtonIcon from '../../assets/icon/more-vertical.png'
 
 function DebateTopic() {
+
+  // Close the dropdown menu if the user clicks outside of it
+  // window.onclick = function(event) {
+  //   if (!event.target.matches('.debate-topic-option-dropdown')) {
+  //     var dropdowns = document.getElementsByClassName("debate-topic-dropdown-content");
+  //     var i;
+  //     for (i = 0; i < dropdowns.length; i++) {
+  //       var openDropdown = dropdowns[i];
+  //       if (openDropdown.classList.contains('show')) {
+  //         openDropdown.classList.remove('show');
+  //       }
+  //     }
+  //   }
+  // }
+  const topicData = {
+    topicName: "รถพลังงานไฟฟ้าจะมาแทนที่แบบสันดาป",
+    topicCreator: "User",
+    topicDescription: ""
+  }
+
+  const dropDownClick = () =>{
+    console.log("clicked")
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
   return (
     <>
         <UserNavbar />
+        {/* page container */}
         <div className="debate-topic-page-container">
-           
+           {/* topic meta data box */}
+          <div className="debate-topic-meta-data-container">
+            {/* left content */}
+            <div className='debate-topic-meta-data-left-content'>
+              {/* topic name */}
+              <p className='debate-topic-topic-name'>{topicData.topicName}</p>
+              {/* topic tag */}
+              <p className='debate-topic-label'>แท็กที่เกี่ยวข้อง:</p>
+
+              <div className='debate-topic-tag-container'>
+                <TopicTag tagName="เทคโนโลยี"/>
+                <TopicTag tagName="เซมิคอนดัคเตอร์"/>
+                
+
+              </div>
+              {/* topic creator row */}
+              <label className='debate-topic-label'>สร้างโดย: </label>
+              <a className='debate-topic-topic-creator-link'>{topicData.topicCreator}</a>
+            </div>
+            {/* right content */}
+            <div className="debate-topic-meta-data-right-content">
+              <p className='debate-topic-topic-description'>คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย คำอธิบาย</p>
+              
+            </div>
+            
+            {/* drop down button */}
+            <div className="debate-topic-option-dropdown">
+              <button className="debate-topic-option-vert-button" onClick={dropDownClick}>
+                <img className='debate-topic-option-image' src={optionButtonIcon} alt="" />
+              </button>
+              <div id="myDropdown" class="debate-topic-dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+
+          </div>
+          {/* end of meta data part */}
+
+          {/* topic content part */}
+          <div className="debate-topic-two-side-container">
+            {/* left contaienr */}
+            <div className="debate-topic-side-box">
+              <div className="debate-topic-side-content-container">
+                <p className='debate-topic-side-stance-title'>
+                  ฝั่งที่เห็นด้วย
+                </p>
+                
+              </div>
+            </div>
+            
+          </div>
         </div>
     </>
   )
