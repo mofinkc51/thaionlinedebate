eb3ea5aed4b6cd201f853877519eb63b8a72579c
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-10-18 23:04:40 +0700

    create admin manage problem page

---
+++ thai-online-debate/src/pages/admin-menu-page/AdminManageProblem.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import AdminManageProblemRow from '../../components/admin-manage-problem-row/AdminManageProblemRow'
+
+function AdminManageProblem() {
+  return (
+    <>
+        <div className="admin-manage-activity-title-row">
+            <h2 className='admin-manage-activity-title'>จัดการปัญหา</h2>
+        </div>
+        {/* table part */}
+        <table className='admin-download-activity-table'>
+
+            {/* table header  */}
+            <tr className='admin-manage-activity-table-header'>
+                <th >ชื่อผู้ร้อง</th>
+                <th>วัน-เวลา</th>
+                <th>รายละเอียดปัญหา</th>
+                <th>สถานะ</th>
+                <th>จัดการ</th>
+            </tr>
+            {/* table body */}
+            <AdminManageProblemRow/>
+        </table>
+    </>
+  )
+}
+
+export default AdminManageProblem
