f3b9d00a6b471450cbbd366e766a3da75fe24c2e
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-05 01:03:21 +0700

    AdminManageUser
    +State สำหรับเก็บข้อความค้นหา
    +fetch('http://localhost:8800/api/users/findall')
    +filteredUsers.map
    +const filteredUsers = users.filter((user)

--- thai-online-debate/src/pages/admin-menu-page/AdminManageUser.jsx
+++ thai-online-debate/src/pages/admin-menu-page/AdminManageUser.jsx
@@ -1,61 +1,65 @@
-import React from 'react'
-import searchIcon from '../../assets/icon/search.png'
-import './AdminManageUser.css'
-import AdminManageUserRow from '../../components/admin-manage-user-row/AdminManageUserRow'
+import React, { useEffect, useState } from 'react';
+import AdminManageUserRow from '../../components/admin-manage-user-row/AdminManageUserRow';
+import searchIcon from '../../assets/icon/search.png'; // เพิ่ม import นี้
+import './AdminManageUser.css' ;
 
 
-function AdminManageUser() {
+const AdminManageUser = () => {
+const [filteredUsers, setFilteredUsers] = useState([]); // เพิ่มตัวแปรนี้
+  const [users, setUsers] = useState([]);
+  const [searchText, setSearchText] = useState(''); // State สำหรับเก็บข้อความค้นหา
+
+  useEffect(() => {
+    fetch('http://localhost:8800/api/users/findall')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // ใน useEffect นี้คุณสามารถกรองข้อมูลผู้ใช้ตามข้อความค้นหาที่ได้รับ
+    const filteredUsers = users.filter((user) =>
+      user.user_name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    // อัพเดทข้อมูลผู้ใช้ที่แสดงในตาราง
+    // โดยใช้ข้อมูลผู้ใช้ที่ถูกกรองเท่านั้น
+    setFilteredUsers(filteredUsers);
+  }, [searchText, users]);
+
   return (
     <>
-        <div className="admin-manage-user-title-row">
-            <h2>จัดการบัญชีผู้ใช้</h2>
-            <div className="admin-manage-user-search-container">
-                <button> <img src={searchIcon} alt="" /></button>
-                <input type="text" placeholder="ค้นหาบัญชีผู้ใช้" />
-            </div>
-            
+      {/* ... */}
+      <div className="admin-manage-user-title-row">
+        <h2>จัดการบัญชีผู้ใช้</h2>
+        <div className="admin-manage-user-search-container">
+          <button> <img src={searchIcon} alt="" /></button>
+          <input
+            type="text"
+            placeholder="ค้นหาบัญชีผู้ใช้"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
         </div>
-        <table className='admin-manage-user-table'>
-            {/* table header  */}
-            <tr className='admin-manage-user-table-header'>
-                <th>ชื่อบัญชีผู้ใช้</th>
-                <th>อีเมล</th>
-                <th>สถานะ</th>
-                <th>ตรวจสอบ</th>
-            </tr>
-            {/* table body */}
-            {<AdminManageUserRow/>}
-            {/* <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/>
-            <AdminManageUserRow/> */}
-
-            
-
-
-            
-
-            
-        </table>
+      </div>
+      <table className='admin-manage-user-table'>
+        {/* table header */}
+        <tr className='admin-manage-user-table-header'>
+          <th>ชื่อบัญชีผู้ใช้</th>
+          <th>อีเมล</th>
+          <th>สถานะ</th>
+          <th>ตรวจสอบ</th>
+        </tr>
+        {/* table body */}
+        {filteredUsers.map((user) => (
+          <AdminManageUserRow key={user.user_id} user={user} />
+        ))}
+      </table>
     </>
-  )
-}
+  );
+};
 
-export default AdminManageUser
+export default AdminManageUser;
