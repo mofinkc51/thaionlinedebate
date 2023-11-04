33724e602a6f0c917901c7ae5a08117890236d61
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-05 00:57:32 +0700

    user.js
    +router.get("/findall", getAllUsers);

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getUser , updateUser} from "../controllers/user.js";
+import { getUser, updateUser } from "../controllers/user.js";
+import { getAllUsers } from "../controllers/get_user.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/find/:user_id",getUser)
-router.put("/edit/:id", updateUser)
+router.get("/find/:user_id", getUser);
+router.put("/edit/:id", updateUser);
+router.get("/findall", getAllUsers);
 
-export default router
+export default router;
