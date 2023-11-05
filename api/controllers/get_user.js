71cb1110be3f8420ce04c80a377e2f03f8a11986
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-06 01:55:33 +0700

    get_user.js
    +API get_user เพื่อดึงข้อมูลของ user ผ่านฟังชัน getAllUsers

---
+++ api/controllers/get_user.js
@@ -0,0 +1,24 @@
+import { db } from "../connect.js";
+
+export const getAllUsers = (req, res) => {
+  const sql = "SELECT user_name,user_email FROM user";
+
+  db.query(sql, (err, data) => {
+    if (err) {
+      console.log("Error:", err);
+      return res.status(500).json(err);
+    }
+
+    if (data.length === 0) {
+      console.log("No users found");
+      return res.status(404).json("No users found");
+    }
+
+    const users = data.map(user => {
+      const { password, ...info } = user;
+      return info;
+    });
+
+    return res.status(200).json(users);
+  });
+};
