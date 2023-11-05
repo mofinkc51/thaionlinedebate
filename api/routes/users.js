9c06738f2849152cf7873507b24490a452bebafb
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-06 01:56:39 +0700

    user.js
    - ลบ router.get("/findall", getAllUsers); ไปอยู่ในส่วนของ admin.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,8 @@
 import express from "express";
 import { getUser, updateUser } from "../controllers/user.js";
-import { getAllUsers } from "../controllers/get_user.js";
 
 const router = express.Router();
-
 router.get("/find/:user_id", getUser);
 router.put("/edit/:id", updateUser);
-router.get("/findall", getAllUsers);
 
 export default router;
