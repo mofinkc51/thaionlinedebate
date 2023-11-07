f89cdde0f17291c506803f99d0796d0acb186a9d
mofinkc51 <82575774+mofinkc51@users.noreply.github.com>
2023-11-08 02:26:47 +0700

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -3,7 +3,7 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"0869844339bB@",
-    database:"thai-online-debate",
+    password:"!@#mofin123",
+    database:"thaionlinedebate"
     port:3306
 })
