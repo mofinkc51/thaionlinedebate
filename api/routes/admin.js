f7a84b9c5f24f1573d88352eeccc50e795e479b1
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-07 22:55:40 +0700

    admin.js
    +getActivity

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { getAllUsers } from "../controllers/get_user.js";
 import { getApprove } from "../controllers/get_apvDownload.js";
+import { getActivity } from "../controllers/get_activity.js";
 
 const router = express.Router();
 
 router.get("/findall", getAllUsers);
 router.get("/apvdownload", getApprove);
+router.get("/activity", getActivity)
 
 export default router;
