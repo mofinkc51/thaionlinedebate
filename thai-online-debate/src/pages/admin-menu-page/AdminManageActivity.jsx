60e6ccdcbcedd2ae30f8ceb948b17aac831efe4d
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-07 22:58:43 +0700

    AdminManageActivity.jsx
    +เรียกใช้ข้อมูลจาก database
    +สามารถใช้ฟังชัน CreateActivityPopup ได้

--- thai-online-debate/src/pages/admin-menu-page/AdminManageActivity.jsx
+++ thai-online-debate/src/pages/admin-menu-page/AdminManageActivity.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './AdminManageActivity.css'
 import AdminManageActivityRow from '../../components/admin-manage-activity-row/AdminManageActivityRow'
 import CreateActivityPopup from '../../components/admin-popup/CreateActivityPopup'
@@ -6,11 +7,33 @@ import EditActivityPopup from '../../components/admin-popup/EditActivityPopup'
 import DeleteActivityPopup from '../../components/admin-popup/DeleteActivityPopup'
 
 function AdminManageActivity() {
+
+  const [activities, setActivities] = useState([]);
+  const [showCreatePopup, setShowCreatePopup] = useState(false);
+
+  useEffect(() => {
+    const fetchActivities = async () => {
+      try {
+        // ทำการเปลี่ยน URL เป็น endpoint ของคุณที่ใช้เรียกข้อมูล
+        const response = await axios.get('http://localhost:8800/api/admin/activity');
+        setActivities(response.data);
+      } catch (error) {
+        console.error('Error fetching activities:', error);
+        // จัดการ error ตามที่ต้องการ
+      }
+    };
+
+    fetchActivities();
+  }, []); // ขึ้นอยู่กับ dependency array ที่ว่าง เพื่อให้เรียกข้อมูลเมื่อ component mount
+
+  const handleCreateButtonClick = () => {
+    setShowCreatePopup(true);
+  };
   return (
     <>
       <div className="admin-manage-activity-title-row">
         <h2 className='admin-manage-activity-title'>จัดการกิจกรรมโต้แย้ง</h2>
-        <button>สร้างกิจกรรมโต้แย้ง</button>
+        <button onClick={() => setShowCreatePopup(true)}>สร้างกิจกรรมโต้แย้ง</button>
       </div>
 
       {/* table part */}
@@ -27,9 +50,11 @@ function AdminManageActivity() {
         </tr>
 
         {/* table body */}
-        <AdminManageActivityRow/>
+        {activities.map((activity) => (
+        <AdminManageActivityRow key={activity.act_id} activity={activity} />
+      ))}
       </table>
-      {/* <CreateActivityPopup/> */}
+      {showCreatePopup && <CreateActivityPopup closePopup={() => setShowCreatePopup(false)} />}
       {/* <EditActivityPopup/> */}
       {/*<DeleteActivityPopup/>*/}
 
