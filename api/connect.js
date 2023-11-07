00e94bcee0834dda9c7050dfb2f3b7dc7635c560
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-07 22:53:57 +0700

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -1,7 +1,7 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host:"127.0.0.1",
+    host:"localhost",
     user:"root",
     password:"0869844339bB@",
     database:"thai-online-debate",
