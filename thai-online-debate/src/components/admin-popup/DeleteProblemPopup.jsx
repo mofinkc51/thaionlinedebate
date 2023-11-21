15c3eb000e5073e384966de466d72d097e1216ea
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-21 21:10:40 +0700

    DeleteProblemPopup.jsx
    เพิ่มการลบคำร้องเรียนปัญหาออกจากหน้าเว็บได้เมื่อกดลบจะเป็นการซ้อนข้อมูลแทน

--- thai-online-debate/src/components/admin-popup/DeleteProblemPopup.jsx
+++ thai-online-debate/src/components/admin-popup/DeleteProblemPopup.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import closeButtonIcon from '../../assets/icon/close.png'
-function DeleteProblemPopup() {
+import React, { useEffect, useState } from 'react';
+import closeButtonIcon from '../../assets/icon/close.png';
+import axios from 'axios';
+
+function DeleteProblemPopup({ onCancel, onConfirm, rp_id }) {
+  const [problems, setProblems] = useState([]);
+
+  const handleConfirmClick = () => {
+    onConfirm();
+  };
+  useEffect(() => {
+    axios.put('http://localhost:8800/api/admin/reportproblem_status', { rp_id })
+      .then((response) => {
+        setProblems(response.data);
+      })
+      .catch((error) => {
+        console.error('เกิดข้อผิดพลาดในการดึงข้อมูล:', error);
+      });
+  }, [rp_id]);
   return (
     <>
-        <div className="delete-activity-bg-opacity">
-            <div className="delete-activity-popup-box">
-                <button className='delete-activity-close-button'><img src={closeButtonIcon} alt="" /></button>
+      <div className="delete-activity-bg-opacity">
+        <div className="delete-activity-popup-box">
+          <button className='delete-activity-close-button' onClick={onCancel}><img src={closeButtonIcon} alt="" /></button>
 
-                <div className="delete-activity-popup-container">
-                    <h3 className='delete-activity-popup-title'>ยืนยันการลบปัญหา</h3>
-                    {/* button row */}
-                    <div className="delete-activity-button-row">
-                        <button className='delete-activity-confirm-button'>ยืนยัน</button>
-                        <button className='delete-activity-cancel-button'>ยกเลิก</button>
-                    </div>
-                </div>
+          <div className="delete-activity-popup-container">
+            <h3 className='delete-activity-popup-title'>ยืนยันการลบปัญหา</h3>
+            {/* Button row */}
+            <div className="delete-activity-button-row">
+              <button className='delete-activity-confirm-button' onClick={handleConfirmClick}>ยืนยัน</button>
+              <button className='delete-activity-cancel-button' onClick={onCancel}>ยกเลิก</button>
             </div>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default DeleteProblemPopup
+export default DeleteProblemPopup;
