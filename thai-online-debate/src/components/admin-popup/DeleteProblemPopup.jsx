bb2544bfba4631f748dd8807025f44e006cb45c1
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-21 23:50:05 +0700

    DeleteProblemPopup.jsx
    ล่าสุดแก้ไข useEffect

--- thai-online-debate/src/components/admin-popup/DeleteProblemPopup.jsx
+++ thai-online-debate/src/components/admin-popup/DeleteProblemPopup.jsx
@@ -1,34 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import closeButtonIcon from '../../assets/icon/close.png';
 import axios from 'axios';
 
 function DeleteProblemPopup({ onCancel, onConfirm, rp_id }) {
-  const [problems, setProblems] = useState([]);
 
-  const handleConfirmClick = () => {
-    onConfirm();
+  const handleConfirmClick = async () => {
+    try {
+      const response = await axios.put('http://localhost:8800/api/admin/reportproblem_status', { rp_id });
+      onConfirm();
+    } catch (error) {
+      console.error('Error updating status:', error);
+    
+    }
   };
-  useEffect(() => {
-    axios.put('http://localhost:8800/api/admin/reportproblem_status', { rp_id })
-      .then((response) => {
-        setProblems(response.data);
-      })
-      .catch((error) => {
-        console.error('เกิดข้อผิดพลาดในการดึงข้อมูล:', error);
-      });
-  }, [rp_id]);
+
+  
+  const handleCancelClick = () => {
+    onCancel(); 
+  };
+  
   return (
     <>
       <div className="delete-activity-bg-opacity">
         <div className="delete-activity-popup-box">
-          <button className='delete-activity-close-button' onClick={onCancel}><img src={closeButtonIcon} alt="" /></button>
+          <button className='delete-activity-close-button' onClick={handleCancelClick}>
+            <img src={closeButtonIcon} alt="" />
+          </button>
 
           <div className="delete-activity-popup-container">
             <h3 className='delete-activity-popup-title'>ยืนยันการลบปัญหา</h3>
             {/* Button row */}
             <div className="delete-activity-button-row">
               <button className='delete-activity-confirm-button' onClick={handleConfirmClick}>ยืนยัน</button>
-              <button className='delete-activity-cancel-button' onClick={onCancel}>ยกเลิก</button>
+              <button className='delete-activity-cancel-button' onClick={handleCancelClick}>ยกเลิก</button>
             </div>
           </div>
         </div>
