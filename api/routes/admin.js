387631b798040fd4194572a67609748dd6ff0432
Masterboom-tha <pansawut.puja@bumail.net>
2023-11-21 21:08:57 +0700

    admin.js (routes)

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,14 +1,20 @@
 import express from "express";
-import { getAllUsers } from "../controllers/get_user.js";
-import { getApprove } from "../controllers/get_apvDownload.js";
-import { getActivity } from "../controllers/get_activity.js";
-import { getProblem } from "../controllers/get_problem.js";
+import { getAllUsers , getActivity, getApprove, getProblem,  updateStatus, downloadRequest, getApprovefromdr_id, postActivity, reportupdateStatus, admindescription} from "../controllers/admin.js";
+
 
 const router = express.Router();
 
 router.get("/findall", getAllUsers)
 router.get("/apvdownload", getApprove)
+router.get("/apvdownload/:dr_id", getApprovefromdr_id)
 router.get("/activity", getActivity)
 router.get("/reportproblem" ,getProblem)
+router.put("/updatestatus",updateStatus)
+router.put("/handlerequest",downloadRequest)
+router.post("/postactivity",postActivity)
+router.put("/reportproblem_status", reportupdateStatus);
+router.put("/rereportproblem_admindescription",admindescription)
+
+
 
 export default router;
