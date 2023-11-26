f12309afc323e8f1ef366745063c1febf4de3086
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-11-26 22:12:03 +0700

    update navbar
    - responsive navbar

--- thai-online-debate/src/components/Navbar/UserNavBar.jsx
+++ thai-online-debate/src/components/Navbar/UserNavBar.jsx
@@ -53,18 +53,48 @@ function UserNavBar() {
         });
       };
 
+      function toggleHam() {
+        const x = document.querySelector(".ham-menu");
+        x.classList.toggle("change");
+    
+        let myMenu = document.getElementById('myMenu');
+        if (myMenu.className === 'nav-vertical-menu') {
+            myMenu.className += ' menu-active';
+        } else {
+            myMenu.className = 'nav-vertical-menu';
+        }
+      }
+
   return (
     <>
        <nav>
             <div class="nav-container">
                 <div class="nav-wrapper">
-                    <div class="logo">
-                        <a href="/"><h3>Thai Online Debate</h3></a>
+                    <div className="nav-logo-ham-row">
+                        <div class="logo">
+                            <a href="/"><h3>Thai Online Debate</h3></a>
+                        </div>
+                        {/* hamburger menu */}
+                        <div class="ham-menu" onClick={toggleHam}>
+                            <div class="bar1"></div>
+                            <div class="bar2"></div>
+                            <div class="bar3"></div>
+                        </div>
                     </div>
-                    <ul class="nav-menu" id="myMenu">
+                    {/* reponsive menu */}
+                    <ul class="nav-vertical-menu" id="myMenu">
+                        <li><a href="/">หน้าแรก</a></li>
+                        <li><a href="/profile/${currentUser.user_id}">โปรไฟล์</a></li>
+                        <li><a href="/fav">รายการประเด็นโต้แย้งที่ชื่นชอบ</a></li>
+                        <li><a href="/downloadrequest">ส่งคำร้องการดาวน์โหลด</a></li>
+                        <li><a href="#" onClick={handleTagClicked}>ประวัติคำร้องขอชุดข้อมูล</a></li>
+                        <li><a href="/" onClick={logout}>ออกจากระบบ</a></li>
+                    </ul>
+                    {/* default menu */}
+                    <ul class="nav-menu">
                         <li><a href="/">หน้าแรก</a></li>
                         {location.pathname === "/" && (
-                            <li><a onClick={createTopicForm}>สร้าง</a></li>
+                            <li className="nav-menu-home"><a onClick={createTopicForm}>สร้าง</a></li>
                         )}
                         <div class="nav-dropdown">
                             <li className='nav-dropdown'><a class="nav-dropbtn" href="">{currentUser.user_name} 
@@ -80,6 +110,7 @@ function UserNavBar() {
                         </div>
 
                     </ul>
+                    
                 </div>
             </div>
         </nav> 
