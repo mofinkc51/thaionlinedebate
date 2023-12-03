95c15623dc801b07f84e1a31248adf721808a0a8
Masterboom-tha <pansawut.puja@bumail.net>
2023-12-03 23:17:48 +0700

    AdminManageUser.jsx
    AdminManageUser.jsx

--- thai-online-debate/src/pages/admin-menu-page/AdminManageUser.jsx
+++ thai-online-debate/src/pages/admin-menu-page/AdminManageUser.jsx
@@ -5,80 +5,70 @@ import './AdminManageUser.css' ;
 import { useNavigate } from 'react-router-dom';
 import { makeRequest } from '../../axios';
 
+const API_ENDPOINT = 'admin/findall';
+
 const AdminManageUser = () => {
   const [filteredUsers, setFilteredUsers] = useState([]);
   const [users, setUsers] = useState([]);
-  const [searchText, setSearchText] = useState(''); // State สำหรับเก็บข้อความค้นหา
+  const [searchText, setSearchText] = useState('');
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
   const navigate = useNavigate();
+
+  useEffect(() => {
+    getUserData();
+  }, [searchText]);
+
+  const getUserData = async () => {
+    try {
+      const response = await makeRequest.get(`${API_ENDPOINT}?search=${searchText}`);
+      setUsers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   const handleInspectUser = (userId) => {
     navigate(`/profile/${userId}`);
   };
-  
-    
-const getUserData = async () => {
-  try {
-    const response = await makeRequest.get('admin/findall')
-    setUsers(response.data);
-  } catch (error) {
-    console.error(error);
-  
-  }
-}
 
-  useEffect(() => {
-    const filteredUsers = users.filter((user) =>
-      user.user_name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    setFilteredUsers(filteredUsers);
-  }, [searchText, users]);
+  const handleHeaderClick = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
 
-  useEffect(() => {
-    getUserData();
-  },[])
-    //sort state and function
-    const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
-
-    const handleHeaderClick = (key) => {
-      let direction = 'ascending';
-      if (sortConfig.key === key && sortConfig.direction === 'ascending') {
-        direction = 'descending';
+  const renderSortArrow = (key) => {
+    if (sortConfig.key === key) {
+      if (sortConfig.direction === 'ascending') {
+        return '▲'; 
       }
-      setSortConfig({ key, direction });
-    };
-    
-    // Function to render the sort arrow based on the current sort configuration
-    const renderSortArrow = (key) => {
-      if (sortConfig.key === key) {
-        if (sortConfig.direction === 'ascending') {
-          return '▲'; 
+      if (sortConfig.direction === 'descending') {
+        return '▼'; 
+      }
+    }
+    return '▼';
+  };
+
+  const sortedusers = useMemo(() => {
+    let sortableItems = [...users];
+    if (sortConfig.key !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
         }
-        if (sortConfig.direction === 'descending') {
-          return '▼'; 
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
         }
-      }
-      return '▼';
-    };
-    // A basic sorting function
-    const sortedusers = useMemo(() => {
-      let sortableItems = [...users]; // Copy the users array to avoid mutating the original state
-      if (sortConfig.key !== null) {
-        sortableItems.sort((a, b) => {
-          if (a[sortConfig.key] < b[sortConfig.key]) {
-            return sortConfig.direction === 'ascending' ? -1 : 1;
-          }
-          if (a[sortConfig.key] > b[sortConfig.key]) {
-            return sortConfig.direction === 'ascending' ? 1 : -1;
-          }
-          return 0;
-        });
-      }
+        return 0;
+      });
+    }
     return sortableItems;
-    }, [users, sortConfig]);
+  }, [users, sortConfig]);
 
   return (
     <>
-      {/* ... */}
       <div className="admin-manage-user-title-row">
         <h2>จัดการบัญชีผู้ใช้</h2>
         <div className="admin-manage-user-search-container">
@@ -92,20 +82,18 @@ const getUserData = async () => {
         </div>
       </div>
       <table className='admin-manage-user-table'>
-        {/* table header */}
         <tr className='admin-manage-user-table-header'>
           <th onClick={() => handleHeaderClick('user_name')}>ชื่อผู้ใช้ {renderSortArrow('user_name')}</th>
           <th onClick={() => handleHeaderClick('user_email')}>อีเมล {renderSortArrow('user_email')}</th>
           <th onClick={() => handleHeaderClick('user_status')}>สถานะ {renderSortArrow('user_status')}</th>
           <th>ตรวจสอบ</th>
         </tr>
-        {/* table body */}
         {sortedusers.map((user) => (
           <AdminManageUserRow key={user.user_id} user={user} onInspect={handleInspectUser} />
         ))}
       </table>
     </>
-
-);
+  );
 };
+
 export default AdminManageUser;
