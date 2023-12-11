82db0aa488586b4ba56c672b185d57b7f95db178
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-12-11 16:00:28 +0700

    update detail title
    - change title text

--- thai-online-debate/src/pages/admin-menu-page/AdminManageRequest.jsx
+++ thai-online-debate/src/pages/admin-menu-page/AdminManageRequest.jsx
@@ -4,10 +4,10 @@ import "./AdminManageRequest.css";
 import { makeRequest } from "../../axios";
 import Swal from "sweetalert2";
 function AdminManageRequest(props) {
-  const { data , onBack ,debate,refresh} = props;
+  const { data, onBack, debate, refresh } = props;
   const timestamp = data.dr_timestamp;
   const dateObject = new Date(timestamp);
-  
+
   const formattedDate = new Intl.DateTimeFormat("th-TH", {
     year: "numeric",
     month: "2-digit",
@@ -17,14 +17,13 @@ function AdminManageRequest(props) {
     second: "2-digit",
   }).format(dateObject);
   const detailData = {
-    dr_id : data.dr_id,
+    dr_id: data.dr_id,
     dr_name: data.dr_name,
     dr_timestamp: formattedDate,
     dr_desc: data.dr_desc,
     dr_proof_one: data.dr_proof_one,
     dr_proof_two: data.dr_proof_two,
   };
-
 
   const storedUserEmail = localStorage.getItem("user");
   const userObject = JSON.parse(storedUserEmail);
@@ -62,68 +61,73 @@ function AdminManageRequest(props) {
     // Extract dr_id from detailData.dr_name
     const drId = detailData.dr_id;
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "อนุมัติคำร้องขอดาวน์โหลดชุดข้อมูล",
-      icon: 'info',
+      icon: "info",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: 'grey',
-      confirmButtonText: 'ใช่ ยืนยัน!',
-      cancelButtonText: 'ยกเลิก'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "grey",
+      confirmButtonText: "ใช่ ยืนยัน!",
+      cancelButtonText: "ยกเลิก",
     }).then(async (result) => {
       if (result.isConfirmed) {
         try {
           // Send the approval request to the server with user_email
-          const response = await makeRequest.post("/admin/adminapvdownload", {
-            dr_id: drId,
-            user_email: userEmail,
-          }).then(() => {
-            onBack();
-            refresh();
-          })
+          const response = await makeRequest
+            .post("/admin/adminapvdownload", {
+              dr_id: drId,
+              user_email: userEmail,
+            })
+            .then(() => {
+              onBack();
+              refresh();
+            });
         } catch (error) {
-            Swal.fire({
-              icon: "error",
-              title: error.response.data
+          Swal.fire({
+            icon: "error",
+            title: error.response.data,
           });
         }
       }
-    })
+    });
   };
-    
+
   const handleDisapproval = async () => {
     const drId = detailData.dr_id; // Extract dr_id from detailData
 
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "ปฎิเสธคำร้องขอดาวน์โหลดชุดข้อมูล",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#d33',
-      cancelButtonColor: 'grey',
-      confirmButtonText: 'ใช่ ยืนยัน!',
-      cancelButtonText: 'ยกเลิก'
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "grey",
+      confirmButtonText: "ใช่ ยืนยัน!",
+      cancelButtonText: "ยกเลิก",
     }).then(async (result) => {
       if (result.isConfirmed) {
-          try {
-            // Send the disapproval request to the server with user_email
-            const response = await makeRequest.post("/admin/rejectedactivity", {
+        try {
+          // Send the disapproval request to the server with user_email
+          const response = await makeRequest
+            .post("/admin/rejectedactivity", {
               dr_id: drId,
               user_email: userEmail,
-            }).then(() => {
+            })
+            .then(() => {
               onBack();
               refresh();
-            })
-          } catch (error) {
-            Swal.fire({
-              icon: "error",
-              title: error.response ? error.response.data : "An unknown error occurred"
+            });
+        } catch (error) {
+          Swal.fire({
+            icon: "error",
+            title: error.response
+              ? error.response.data
+              : "An unknown error occurred",
           });
         }
       }
-    })
+    });
   };
-  
 
   return (
     <>
@@ -144,11 +148,14 @@ function AdminManageRequest(props) {
         </div>
         {/* topic requested row */}
         <div className="admin-manage-request-topic-requested-row">
-          <h4>ชุดข้อมูลที่ต้องการดาวน์โหลด</h4>
-          <p className="manage-request-text">                    
-          {props.debate.map((debate, index) => (
-              <p key = {index} className='download-detail-text'>{debate.dbt_title}</p>
-          ))}</p>
+          <h4>หัวข้อชุดข้อมูลที่ต้องการดาวน์โหลด</h4>
+          <p className="manage-request-text">
+            {props.debate.map((debate, index) => (
+              <p key={index} className="download-detail-text">
+                {debate.dbt_title}
+              </p>
+            ))}
+          </p>
         </div>
         {/* reason row */}
         <div className="admin-manage-request-topic-reason-row">
@@ -183,14 +190,17 @@ function AdminManageRequest(props) {
         <div className="admin-manage-request-button-row">
           <button
             className="manage-request-confirm-button"
-            onClick={handleApproval}>
+            onClick={handleApproval}
+          >
             อนุมัติ
           </button>
 
-          <button className="manage-request-cancel-button"
-            onClick={handleDisapproval}>
+          <button
+            className="manage-request-cancel-button"
+            onClick={handleDisapproval}
+          >
             ไม่อนุมัติ
-            </button>
+          </button>
         </div>
       </div>
     </>
