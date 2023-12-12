581532f363bc07fa6f56a861612133757d316a1c
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-12-12 13:25:30 +0700

    Update SignUp.jsx
    - update religion sequence

--- thai-online-debate/src/pages/signup-page/SignUp.jsx
+++ thai-online-debate/src/pages/signup-page/SignUp.jsx
@@ -122,120 +122,130 @@ function SignUp() {
               <div className="signup-dob-reli-row">
                 <div>
                   <p class="signup-label">วันเดือนปีเกิด</p>
-                  <input type="date" id="birthday" name="birthday" className="signup-birthday-input"></input>
+                  <input
+                    type="date"
+                    id="birthday"
+                    name="birthday"
+                    className="signup-birthday-input"
+                  ></input>
                 </div>
 
                 <div>
                   <p class="signup-label">เพศ</p>
                   <select name="province" className="signup-gender-input">
-                    <option value="" selected>เลือกเพศ</option>
+                    <option value="" selected>
+                      เลือกเพศ
+                    </option>
                     <option value="ชาย">เพศชาย</option>
                     <option value="หญิง">เพศหญิง</option>
                     <option value="ไม่ระบุ">ไม่ระบุ</option>
                   </select>
                 </div>
-                
               </div>
 
               {/*  */}
 
               <div className="signup-dob-reli-row">
-                  <div>
+                <div>
                   <p class="signup-label">ศาสนา</p>
-                    <select name="province" className="signup-reli-input">
-                      <option value="" selected>เลือกศาสนา</option>
-                      <option value="ศาสนาพุทธ">ศาสนาพุทธ</option>
-                      <option value="ศาสนาอิสลาม">ศาสนาอิสลาม</option>
-                      <option value="ศาสนาคริสต์">ศาสนาคริสต์</option>
-                      <option value="ศาสนาอื่น ๆ">ศาสนาอื่น ๆ</option>
-                    </select>
-                  </div>
-                  <div>
+                  <select name="province" className="signup-reli-input">
+                    <option value="" selected>
+                      เลือกศาสนา
+                    </option>
+                    <option value="ศาสนาพุทธ">ศาสนาพุทธ</option>
+                    <option value="ศาสนาคริสต์">ศาสนาคริสต์</option>
+                    <option value="ศาสนาอิสลาม">ศาสนาอิสลาม</option>
+                    <option value="ศาสนาอื่น ๆ">ศาสนาอื่น ๆ</option>
+                  </select>
+                </div>
+                <div>
                   <p class="signup-label">จังหวัด</p>
-              <select name="province" className="signup-province-input">
-                <option value="" selected>เลือกจังหวัด</option>
-                <option value="กรุงเทพมหานคร">กรุงเทพมหานคร</option>
-                <option value="กระบี่">กระบี่ </option>
-                <option value="กาญจนบุรี">กาญจนบุรี </option>
-                <option value="กาฬสินธุ์">กาฬสินธุ์ </option>
-                <option value="กำแพงเพชร">กำแพงเพชร </option>
-                <option value="ขอนแก่น">ขอนแก่น</option>
-                <option value="จันทบุรี">จันทบุรี</option>
-                <option value="ฉะเชิงเทรา">ฉะเชิงเทรา </option>
-                <option value="ชัยนาท">ชัยนาท </option>
-                <option value="ชัยภูมิ">ชัยภูมิ </option>
-                <option value="ชุมพร">ชุมพร </option>
-                <option value="ชลบุรี">ชลบุรี </option>
-                <option value="เชียงใหม่">เชียงใหม่ </option>
-                <option value="เชียงราย">เชียงราย </option>
-                <option value="ตรัง">ตรัง </option>
-                <option value="ตราด">ตราด </option>
-                <option value="ตาก">ตาก </option>
-                <option value="นครนายก">นครนายก </option>
-                <option value="นครปฐม">นครปฐม </option>
-                <option value="นครพนม">นครพนม </option>
-                <option value="นครราชสีมา">นครราชสีมา </option>
-                <option value="นครศรีธรรมราช">นครศรีธรรมราช </option>
-                <option value="นครสวรรค์">นครสวรรค์ </option>
-                <option value="นราธิวาส">นราธิวาส </option>
-                <option value="น่าน">น่าน </option>
-                <option value="นนทบุรี">นนทบุรี </option>
-                <option value="บึงกาฬ">บึงกาฬ</option>
-                <option value="บุรีรัมย์">บุรีรัมย์</option>
-                <option value="ประจวบคีรีขันธ์">ประจวบคีรีขันธ์ </option>
-                <option value="ปทุมธานี">ปทุมธานี </option>
-                <option value="ปราจีนบุรี">ปราจีนบุรี </option>
-                <option value="ปัตตานี">ปัตตานี </option>
-                <option value="พะเยา">พะเยา </option>
-                <option value="พระนครศรีอยุธยา">พระนครศรีอยุธยา </option>
-                <option value="พังงา">พังงา </option>
-                <option value="พิจิตร">พิจิตร </option>
-                <option value="พิษณุโลก">พิษณุโลก </option>
-                <option value="เพชรบุรี">เพชรบุรี </option>
-                <option value="เพชรบูรณ์">เพชรบูรณ์ </option>
-                <option value="แพร่">แพร่ </option>
-                <option value="พัทลุง">พัทลุง </option>
-                <option value="ภูเก็ต">ภูเก็ต </option>
-                <option value="มหาสารคาม">มหาสารคาม </option>
-                <option value="มุกดาหาร">มุกดาหาร </option>
-                <option value="แม่ฮ่องสอน">แม่ฮ่องสอน </option>
-                <option value="ยโสธร">ยโสธร </option>
-                <option value="ยะลา">ยะลา </option>
-                <option value="ร้อยเอ็ด">ร้อยเอ็ด </option>
-                <option value="ระนอง">ระนอง </option>
-                <option value="ระยอง">ระยอง </option>
-                <option value="ราชบุรี">ราชบุรี</option>
-                <option value="ลพบุรี">ลพบุรี </option>
-                <option value="ลำปาง">ลำปาง </option>
-                <option value="ลำพูน">ลำพูน </option>
-                <option value="เลย">เลย </option>
-                <option value="ศรีสะเกษ">ศรีสะเกษ</option>
-                <option value="สกลนคร">สกลนคร</option>
-                <option value="สงขลา">สงขลา </option>
-                <option value="สมุทรสาคร">สมุทรสาคร </option>
-                <option value="สมุทรปราการ">สมุทรปราการ </option>
-                <option value="สมุทรสงคราม">สมุทรสงคราม </option>
-                <option value="สระแก้ว">สระแก้ว </option>
-                <option value="สระบุรี">สระบุรี </option>
-                <option value="สิงห์บุรี">สิงห์บุรี </option>
-                <option value="สุโขทัย">สุโขทัย </option>
-                <option value="สุพรรณบุรี">สุพรรณบุรี </option>
-                <option value="สุราษฎร์ธานี">สุราษฎร์ธานี </option>
-                <option value="สุรินทร์">สุรินทร์ </option>
-                <option value="สตูล">สตูล </option>
-                <option value="หนองคาย">หนองคาย </option>
-                <option value="หนองบัวลำภู">หนองบัวลำภู </option>
-                <option value="อำนาจเจริญ">อำนาจเจริญ </option>
-                <option value="อุดรธานี">อุดรธานี </option>
-                <option value="อุตรดิตถ์">อุตรดิตถ์ </option>
-                <option value="อุทัยธานี">อุทัยธานี </option>
-                <option value="อุบลราชธานี">อุบลราชธานี</option>
-                <option value="อ่างทอง">อ่างทอง </option>
-                <option value="อื่นๆ">อื่นๆ</option>
-              </select>
-                  </div>
+                  <select name="province" className="signup-province-input">
+                    <option value="" selected>
+                      เลือกจังหวัด
+                    </option>
+                    <option value="กรุงเทพมหานคร">กรุงเทพมหานคร</option>
+                    <option value="กระบี่">กระบี่ </option>
+                    <option value="กาญจนบุรี">กาญจนบุรี </option>
+                    <option value="กาฬสินธุ์">กาฬสินธุ์ </option>
+                    <option value="กำแพงเพชร">กำแพงเพชร </option>
+                    <option value="ขอนแก่น">ขอนแก่น</option>
+                    <option value="จันทบุรี">จันทบุรี</option>
+                    <option value="ฉะเชิงเทรา">ฉะเชิงเทรา </option>
+                    <option value="ชัยนาท">ชัยนาท </option>
+                    <option value="ชัยภูมิ">ชัยภูมิ </option>
+                    <option value="ชุมพร">ชุมพร </option>
+                    <option value="ชลบุรี">ชลบุรี </option>
+                    <option value="เชียงใหม่">เชียงใหม่ </option>
+                    <option value="เชียงราย">เชียงราย </option>
+                    <option value="ตรัง">ตรัง </option>
+                    <option value="ตราด">ตราด </option>
+                    <option value="ตาก">ตาก </option>
+                    <option value="นครนายก">นครนายก </option>
+                    <option value="นครปฐม">นครปฐม </option>
+                    <option value="นครพนม">นครพนม </option>
+                    <option value="นครราชสีมา">นครราชสีมา </option>
+                    <option value="นครศรีธรรมราช">นครศรีธรรมราช </option>
+                    <option value="นครสวรรค์">นครสวรรค์ </option>
+                    <option value="นราธิวาส">นราธิวาส </option>
+                    <option value="น่าน">น่าน </option>
+                    <option value="นนทบุรี">นนทบุรี </option>
+                    <option value="บึงกาฬ">บึงกาฬ</option>
+                    <option value="บุรีรัมย์">บุรีรัมย์</option>
+                    <option value="ประจวบคีรีขันธ์">ประจวบคีรีขันธ์ </option>
+                    <option value="ปทุมธานี">ปทุมธานี </option>
+                    <option value="ปราจีนบุรี">ปราจีนบุรี </option>
+                    <option value="ปัตตานี">ปัตตานี </option>
+                    <option value="พะเยา">พะเยา </option>
+                    <option value="พระนครศรีอยุธยา">พระนครศรีอยุธยา </option>
+                    <option value="พังงา">พังงา </option>
+                    <option value="พิจิตร">พิจิตร </option>
+                    <option value="พิษณุโลก">พิษณุโลก </option>
+                    <option value="เพชรบุรี">เพชรบุรี </option>
+                    <option value="เพชรบูรณ์">เพชรบูรณ์ </option>
+                    <option value="แพร่">แพร่ </option>
+                    <option value="พัทลุง">พัทลุง </option>
+                    <option value="ภูเก็ต">ภูเก็ต </option>
+                    <option value="มหาสารคาม">มหาสารคาม </option>
+                    <option value="มุกดาหาร">มุกดาหาร </option>
+                    <option value="แม่ฮ่องสอน">แม่ฮ่องสอน </option>
+                    <option value="ยโสธร">ยโสธร </option>
+                    <option value="ยะลา">ยะลา </option>
+                    <option value="ร้อยเอ็ด">ร้อยเอ็ด </option>
+                    <option value="ระนอง">ระนอง </option>
+                    <option value="ระยอง">ระยอง </option>
+                    <option value="ราชบุรี">ราชบุรี</option>
+                    <option value="ลพบุรี">ลพบุรี </option>
+                    <option value="ลำปาง">ลำปาง </option>
+                    <option value="ลำพูน">ลำพูน </option>
+                    <option value="เลย">เลย </option>
+                    <option value="ศรีสะเกษ">ศรีสะเกษ</option>
+                    <option value="สกลนคร">สกลนคร</option>
+                    <option value="สงขลา">สงขลา </option>
+                    <option value="สมุทรสาคร">สมุทรสาคร </option>
+                    <option value="สมุทรปราการ">สมุทรปราการ </option>
+                    <option value="สมุทรสงคราม">สมุทรสงคราม </option>
+                    <option value="สระแก้ว">สระแก้ว </option>
+                    <option value="สระบุรี">สระบุรี </option>
+                    <option value="สิงห์บุรี">สิงห์บุรี </option>
+                    <option value="สุโขทัย">สุโขทัย </option>
+                    <option value="สุพรรณบุรี">สุพรรณบุรี </option>
+                    <option value="สุราษฎร์ธานี">สุราษฎร์ธานี </option>
+                    <option value="สุรินทร์">สุรินทร์ </option>
+                    <option value="สตูล">สตูล </option>
+                    <option value="หนองคาย">หนองคาย </option>
+                    <option value="หนองบัวลำภู">หนองบัวลำภู </option>
+                    <option value="อำนาจเจริญ">อำนาจเจริญ </option>
+                    <option value="อุดรธานี">อุดรธานี </option>
+                    <option value="อุตรดิตถ์">อุตรดิตถ์ </option>
+                    <option value="อุทัยธานี">อุทัยธานี </option>
+                    <option value="อุบลราชธานี">อุบลราชธานี</option>
+                    <option value="อ่างทอง">อ่างทอง </option>
+                    <option value="อื่น ๆ">อื่น ๆ</option>
+                  </select>
+                </div>
               </div>
-              
+
               {/*  */}
 
               <p class="signup-label">รหัสผ่าน</p>
