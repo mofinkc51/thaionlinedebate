8a3583fd0d93aad636e39df580b4488606c31619
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-12-14 19:13:06 +0700

    update activity component
    - removed text underline

--- thai-online-debate/src/components/ActivityComponent.jsx
+++ thai-online-debate/src/components/ActivityComponent.jsx
@@ -9,7 +9,7 @@ function ActivityComponent(props) {
   const topicData = {
     dbt_id: props.data.dbt_id,
     dbt_title: props.data.dbt_title,
-    dbt_description: props.data.dbt_description
+    dbt_description: props.data.dbt_description,
   };
   const [topicTag, setTopicTag] = useState([]);
   const [isLoading, setIsLoading] = useState(true); // เพิ่ม state สำหรับติดตามสถานะการโหลด
@@ -32,80 +32,80 @@ function ActivityComponent(props) {
   }, [topicTag]);
   return (
     <>
-    <Link to={`/topic/${topicData.dbt_id}`}>
-      <div className="activity-component-meta-data-container">
-        <div className="activity-component-meta-data-content">
-          {/* topic name */}
-          <div className="activity-component-topic-name-row">
-            <img src={eventIcon} alt="" className="activity-component-icon" />
-            <p className="activity-component-topic-name">
-              {topicData.dbt_title}
-            </p>
-          </div>
+      <Link className="activity-link" to={`/topic/${topicData.dbt_id}`}>
+        <div className="activity-component-meta-data-container">
+          <div className="activity-component-meta-data-content">
+            {/* topic name */}
+            <div className="activity-component-topic-name-row">
+              <img src={eventIcon} alt="" className="activity-component-icon" />
+              <p className="activity-component-topic-name">
+                {topicData.dbt_title}
+              </p>
+            </div>
 
-          {/* desc row */}
-          {/* <h3 className="activity-component-topic-description-title">
+            {/* desc row */}
+            {/* <h3 className="activity-component-topic-description-title">
               รายละเอียดประเด็นโต้แย้ง
             </h3> */}
-          <div className="activity-component-description-box">
-            <p className="activity-component-topic-description">
-              {topicData.dbt_description}
-            </p>
-          </div>
+            <div className="activity-component-description-box">
+              <p className="activity-component-topic-description">
+                {topicData.dbt_description}
+              </p>
+            </div>
 
-          {/* topic tag */}
-          <div className="activity-component-tag-container">
-            {/* <p className="activity-component-label">แท็กที่เกี่ยวข้อง</p> */}
-            {isLoading ? (
-              <p>Loading tags...</p> // แสดงข้อความหรือ spinner ในขณะที่ข้อมูลกำลังโหลด
-            ) : (
-              topicTag.map((tag) => (
-                <TopicTag key={tag.id} tagName={tag.tag_title} /> // ใช้ key prop เพื่อประสิทธิภาพในการ render
-              ))
-            )}
-            {/* <TopicTag tagName="ทดลอง" />
+            {/* topic tag */}
+            <div className="activity-component-tag-container">
+              {/* <p className="activity-component-label">แท็กที่เกี่ยวข้อง</p> */}
+              {isLoading ? (
+                <p>Loading tags...</p> // แสดงข้อความหรือ spinner ในขณะที่ข้อมูลกำลังโหลด
+              ) : (
+                topicTag.map((tag) => (
+                  <TopicTag key={tag.id} tagName={tag.tag_title} /> // ใช้ key prop เพื่อประสิทธิภาพในการ render
+                ))
+              )}
+              {/* <TopicTag tagName="ทดลอง" />
             <TopicTag tagName="ทดลอง" />
             <TopicTag tagName="ทดลอง" />
             <TopicTag tagName="ทดลอง" /> */}
-          </div>
-
-          {/* progress bar */}
-          <div className="activity-component-legend-row">
-            {/* agree legend */}
-            <div className="activity-component-legend-element">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="13"
-                height="13"
-                viewBox="0 0 13 13"
-                fill="none"
-              >
-                <circle cx="6.5" cy="6.5" r="6.5" fill="#0DC7B1" />
-              </svg>
-              <p className="activity-component-legend-text">
-                {/* {topicData.dbt_agree} {Math.round(percentageAgree)}% */}
-              </p>
             </div>
 
-            {/* disagree legend */}
-            <div className="activity-component-legend-element">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="13"
-                height="13"
-                viewBox="0 0 13 13"
-                fill="none"
-              >
-                <circle cx="6.5" cy="6.5" r="6.5" fill="#EB5757" />
-              </svg>
-              <p className="activity-component-legend-text">
-                {/* {topicData.dbt_disagree} {Math.round(percentageDisAgree)}% */}
-              </p>
+            {/* progress bar */}
+            <div className="activity-component-legend-row">
+              {/* agree legend */}
+              <div className="activity-component-legend-element">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="13"
+                  height="13"
+                  viewBox="0 0 13 13"
+                  fill="none"
+                >
+                  <circle cx="6.5" cy="6.5" r="6.5" fill="#0DC7B1" />
+                </svg>
+                <p className="activity-component-legend-text">
+                  {/* {topicData.dbt_agree} {Math.round(percentageAgree)}% */}
+                </p>
+              </div>
+
+              {/* disagree legend */}
+              <div className="activity-component-legend-element">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="13"
+                  height="13"
+                  viewBox="0 0 13 13"
+                  fill="none"
+                >
+                  <circle cx="6.5" cy="6.5" r="6.5" fill="#EB5757" />
+                </svg>
+                <p className="activity-component-legend-text">
+                  {/* {topicData.dbt_disagree} {Math.round(percentageDisAgree)}% */}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
     </>
   );
 }
