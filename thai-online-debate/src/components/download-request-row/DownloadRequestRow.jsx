c966ec8f3ba99d5db1e8302828dbd8a299a6e9dd
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-12-14 19:22:01 +0700

    update request list
    - increased container size
    - align debate topic quantity to center

--- thai-online-debate/src/components/download-request-row/DownloadRequestRow.jsx
+++ thai-online-debate/src/components/download-request-row/DownloadRequestRow.jsx
@@ -1,52 +1,55 @@
-import React from 'react'
-import './DownloadRequestRow.css'
-import deleteButtonIcon from '../../assets/icon/trash.svg'
-import editButtonIcon from '../../assets/icon/edit.svg'
-import { makeRequest } from '../../axios'
+import React from "react";
+import "./DownloadRequestRow.css";
+import deleteButtonIcon from "../../assets/icon/trash.svg";
+import editButtonIcon from "../../assets/icon/edit.svg";
+import { makeRequest } from "../../axios";
 function DownloadRequestRow(props) {
-    const rowData = {
-        topicName: props.data.dbt_title,
-        stanceOne: props.data.dbt_agree,
-        stanceTwo: props.data.dbt_disagree,
-        commentQuantity: props.data.count,
-        dbt_id : props.data.dbt_id
+  const rowData = {
+    topicName: props.data.dbt_title,
+    stanceOne: props.data.dbt_agree,
+    stanceTwo: props.data.dbt_disagree,
+    commentQuantity: props.data.count,
+    dbt_id: props.data.dbt_id,
+  };
+  const deleteLocalStorage = async () => {
+    console.log(rowData.dbt_id);
+    let downloadList = JSON.parse(localStorage.getItem("downloadList"));
+
+    // ตรวจสอบว่า downloadList มีข้อมูลหรือไม่
+    if (downloadList) {
+      downloadList = downloadList.filter((item) => item !== rowData.dbt_id);
+      localStorage.setItem("downloadList", JSON.stringify(downloadList));
     }
-    const deleteLocalStorage = async () => {
-        console.log(rowData.dbt_id);        
-        let downloadList = JSON.parse(localStorage.getItem('downloadList'));
-    
-        // ตรวจสอบว่า downloadList มีข้อมูลหรือไม่
-        if (downloadList) {
-            downloadList = downloadList.filter(item => item !== rowData.dbt_id);
-            localStorage.setItem('downloadList', JSON.stringify(downloadList));
-        }
-    
-        // ไม่ว่า downloadList จะมีข้อมูลหรือไม่ก็ตาม, ดำเนินการลบข้อมูลต่อไป
-        try {
-            const res = await makeRequest.delete(`/downloads/${rowData.dbt_id}`);
-            console.log(res.data);
-        } catch (err) {
-            console.log(err);
-        }
-    
-        props.refresh();
+
+    // ไม่ว่า downloadList จะมีข้อมูลหรือไม่ก็ตาม, ดำเนินการลบข้อมูลต่อไป
+    try {
+      const res = await makeRequest.delete(`/downloads/${rowData.dbt_id}`);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
     }
-    
+
+    props.refresh();
+  };
 
   return (
     <>
-        <tr className='download-request-row'>
-            <td>{rowData.topicName}</td>
-            <td className='download-request-row-stance'>{rowData.stanceOne}</td>
-            <td className='download-request-row-stance'>{rowData.stanceTwo}</td>
-            <td className='download-request-row-number'>{rowData.commentQuantity}</td>
-            <td>
-                {/* <button><img src={editButtonIcon} alt="" /></button> */}
-                <button onClick={deleteLocalStorage} ><img src={deleteButtonIcon}alt="" /></button>
-            </td>
-        </tr>
+      <tr className="download-request-row">
+        <td>{rowData.topicName}</td>
+        <td className="download-request-row-stance">{rowData.stanceOne}</td>
+        <td className="download-request-row-stance">{rowData.stanceTwo}</td>
+        <td className="download-request-row-number">
+          <p>{rowData.commentQuantity}</p>
+        </td>
+        <td>
+          {/* <button><img src={editButtonIcon} alt="" /></button> */}
+          <button onClick={deleteLocalStorage}>
+            <img src={deleteButtonIcon} alt="" />
+          </button>
+        </td>
+      </tr>
     </>
-  )
+  );
 }
 
-export default DownloadRequestRow
+export default DownloadRequestRow;
